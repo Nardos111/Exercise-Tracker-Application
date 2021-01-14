2e6df693031a0b7569cc82671fc567bd6a3c170f
nardos111 <nardos2alemu@gmail.com>
2021-01-14 14:31:53 +0300

    Connect create exercise component to the database

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import DatePicker from "react-datepicker";
+import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-export default class CreateExercises extends Component {
-    constructor(props){
+export default class CreateExercise extends Component {
+  constructor(props) {
     super(props);
 
     this.onChangeUsername = this.onChangeUsername.bind(this);
@@ -13,55 +14,76 @@ export default class CreateExercises extends Component {
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-        username: "",
-        description: "",
-        duration: 0,
-        date: new Date(),
-        users: []
+      username: '',
+      description: '',
+      duration: 0,
+      date: new Date(),
+      users: []
     }
-}
-componentDidMount() {
-    this.setState({
-        users: ["test user"],
-        username: "test user"
-    })
-}
-onChangeUsername(e) {
-    this.setState({
-        username: e.target.value
-    });
-}
-onChangeDescription(e) {
-    this.setState({
-        description: e.target.value
-    });
-}
-onChangeDuration(e) {
-    this.setState({
-        duration: e.target.value
-    });
-}
-onChangeDate(date) {
-    this.setState({
-        date: date
-    });
-}
+  }
 
-onSubmit(e) {
+  componentDidMount() {
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    })
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    })
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    })
+  }
+
+  onSubmit(e) {
     e.preventDefault();
+
     const exercise = {
-        username: this.state.username,
-        description: this.state.description,
-        duration: this.state.duration,
-        date: this.state.date
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
     }
+
     console.log(exercise);
-    window.location = "/"
-}
-    render() {
-        return (
-            <div>
-                <h3>Create New Exercise Log</h3>
+
+    axios.post('http://localhost:5000/exercises/add', exercise)
+      .then(res => console.log(res.data));
+
+    window.location = '/';
+  }
+
+  render() {
+    return (
+    <div>
+      <h3>Create New Exercise Log</h3>
       <form onSubmit={this.onSubmit}>
         <div className="form-group"> 
           <label>Username: </label>
@@ -112,7 +134,7 @@ onSubmit(e) {
           <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
         </div>
       </form>
-            </div>
-        )
-    }
+    </div>
+    )
+  }
 }
